d5e13d0f9af9645e2ac0730ebb3f2e507325b68e
devanshnexus <125756446+devanshnexus@users.noreply.github.com>
2026-07-27 10:17:55 +0530

    Add: palindrome-number - submission-3

---
+++ Data Structures & Algorithms/palindrome-number/submission-3.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+}
